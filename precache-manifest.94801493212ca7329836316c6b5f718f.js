4b386a523a136ecdaf6824bf035f2bf5e3b06ff3
arist <saristide99@gmail.com>
2022-11-12 18:49:39 +0000

    Updates

---
+++ precache-manifest.94801493212ca7329836316c6b5f718f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b379fcc53459c9f7e09",
+    "url": "/hordalhealth/static/css/main.74c75e02.chunk.css"
+  },
+  {
+    "revision": "8b379fcc53459c9f7e09",
+    "url": "/hordalhealth/static/js/main.8b379fcc.chunk.js"
+  },
+  {
+    "revision": "1374bb742acbcc11864e",
+    "url": "/hordalhealth/static/js/1.1374bb74.chunk.js"
+  },
+  {
+    "revision": "8595ac41b6d6ad60217b",
+    "url": "/hordalhealth/static/js/runtime~main.8595ac41.js"
+  },
+  {
+    "revision": "67cdba53cfc39b43770a8d337a20e83c",
+    "url": "/hordalhealth/static/media/health-insurance.67cdba53.png"
+  },
+  {
+    "revision": "44f6b26a1d8c5fef76990d684eb4917a",
+    "url": "/hordalhealth/static/media/profile.44f6b26a.jpg"
+  },
+  {
+    "revision": "587eada7b8fbcfbb01020c8a0fdff868",
+    "url": "/hordalhealth/static/media/j.587eada7.jpeg"
+  },
+  {
+    "revision": "761b9ef5e6391a2cf5fb91abbd5fd942",
+    "url": "/hordalhealth/index.html"
+  }
+];
